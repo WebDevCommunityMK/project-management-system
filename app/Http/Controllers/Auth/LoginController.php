f376f7e2ef090948e3547fb4fffbf9463b3a7235
Slobodan Veselinov <slobodan.veselinov@gmail.com>
2023-10-01 22:41:24 +0200

    added return type to logout method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,7 +42,8 @@ class LoginController extends Controller
 
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request): RedirectResponse
+    {
 
         Auth::logout();
         $request->session()->invalidate();
