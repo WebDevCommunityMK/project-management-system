3c96ab67f376cc3f3ae5effaef3f9f46267af5d9
davorminchorov <davorminchorov@users.noreply.github.com>
2023-10-08 18:14:42 +0000

    Apply ESLint and Prettier code style fixes

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="max-w-7xl mx-auto p-6 flex flex-col justify-center items-center">
-    <div class="flex w-full gap-3">
-      <Link href="/login" class="hover:font-bold">Login</Link>
-      <Link href="/register" class="hover:font-bold">Register</Link>
+    <div
+        class="max-w-7xl mx-auto p-6 flex flex-col justify-center items-center"
+    >
+        <div class="flex w-full gap-3">
+            <Link href="/login" class="hover:font-bold">Login</Link>
+            <Link href="/register" class="hover:font-bold">Register</Link>
+        </div>
+        <h1>Hello Project Managment System Team</h1>
+        <p>This is Vuejs InertiaJs Home View</p>
     </div>
-    <h1>Hello Project Managment System Team</h1>
-    <p>This is Vuejs InertiaJs Home View</p>
-  </div>
 </template>
 
 <script setup></script>
--- resources/js/Pages/Main-Platform/Dashboard.vue
+++ resources/js/Pages/Main-Platform/Dashboard.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="max-w-7xl mx-auto p-6 flex justify-between items-center">
-    Dashboard
+    <div class="max-w-7xl mx-auto p-6 flex justify-between items-center">
+        Dashboard
 
-    <Link href="/logout" method="post">Logout</Link>
-  </div>
+        <Link href="/logout" method="post">Logout</Link>
+    </div>
 </template>
 
 <script setup></script>
