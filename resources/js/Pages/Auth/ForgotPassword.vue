521f1d15166c09966e086b390c44bb7c13b3ba2c
calexandar <alexandercvet243@gmail.com>
2023-10-12 20:12:45 +0200

    aded missin layout

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,32 +1,34 @@
 <template>
-    <div class="bg-[#F9FAFB] h-screen w-screen flex items-center">
-        <div class="h-max mx-auto flex flex-col items-center">          
-            <form @submit.prevent="submit">
-                <div class="bg-white shadow-xl p-10 flex flex-col gap-4 text-sm">
-                    <h1 class="text-xl font-bold text-center pb-4">Reset your password</h1>
-                    <p class="mb-4 text-sm text-gray-600 dark:text-gray-400">Enter your email and we'll send you a link to reset your password.</p>
-                    <div>
-                        <label class="text-gray-600 font-bold inline-block pb-2" for="email">Email</label>
-                        <input
-                            v-model="form.email" 
-                            class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" 
-                            type="email" name="email" 
-                            placeholder="example@mail.com">
-                        <div v-if="form.errors.email" class="text-sm text-red-500 py-2">{{ form.errors.email }}</div>    
+    <Layout>
+        <div class="bg-[#F9FAFB] h-screen w-screen flex items-center">
+            <div class="h-max mx-auto flex flex-col items-center">          
+                <form @submit.prevent="submit">
+                    <div class="bg-white shadow-xl p-10 flex flex-col gap-4 text-sm">
+                        <h1 class="text-xl font-bold text-center pb-4">Reset your password</h1>
+                        <p class="mb-4 text-sm text-gray-600 dark:text-gray-400">Enter your email and we'll send you a link to reset your password.</p>
+                        <div>
+                            <label class="text-gray-600 font-bold inline-block pb-2" for="email">Email</label>
+                            <input
+                                v-model="form.email" 
+                                class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" 
+                                type="email" name="email" 
+                                placeholder="example@mail.com">
+                            <div v-if="form.errors.email" class="text-sm text-red-500 py-2">{{ form.errors.email }}</div>    
+                        </div>
+                        <div>
+                            <input 
+                                class="bg-[#4F46E5] w-full py-2 rounded-md text-white font-bold cursor-pointer hover:bg-[#181196]" 
+                                type="submit" 
+                                value="Email Password Reset Link" >
+                        </div>
+                        <div v-if="status" class="text-center font-medium text-sm text-green-600 dark:text-green-400">
+                        {{ status }}
+                        </div>                    
                     </div>
-                    <div>
-                        <input 
-                            class="bg-[#4F46E5] w-full py-2 rounded-md text-white font-bold cursor-pointer hover:bg-[#181196]" 
-                            type="submit" 
-                            value="Email Password Reset Link" >
-                    </div>
-                    <div v-if="status" class="text-center font-medium text-sm text-green-600 dark:text-green-400">
-                    {{ status }}
-                    </div>                    
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
+    </Layout>
 </template>
 
 <script setup>
@@ -46,5 +48,14 @@ let submit = () => {
     form.post('/forgot-password')
 };
 
+</script>
+
+<script>
+import Layout from './Layouts/Layout.vue'
+
+export default {
+  layout: (h, page) => h(Layout, [page]),
+  layout: Layout,
+}
 </script>
 
--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -1,48 +1,50 @@
 <template>
-    <div class="bg-[#F9FAFB] h-screen w-screen flex items-center">
-        <div class="h-max mx-auto flex flex-col items-center">
-            <h1 class="text-xl font-bold text-center pb-10">Reset your password</h1>
-                <form @submit.prevent="submit">
-                    <div class="bg-white shadow-xl p-10 flex flex-col gap-4 text-sm">
-                        <div>
-                            <label class="text-gray-600 font-bold inline-block pb-2" for="email">Email</label>
-                            <input
-                                v-model="form.email" 
-                                class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" 
-                                type="email" name="email" 
-                                placeholder="example@mail.com">
-                            <div v-if="form.errors.email" class="text-sm text-red-500 py-2">{{ form.errors.email }}</div>    
-                        </div>
-                        <div>
-                            <label class="text-gray-600 font-bold inline-block pb-2" for="password">New Password</label>
-                            <input 
-                                v-model="form.password"
-                                class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" 
-                                type="password" name="password" 
-                                placeholder="******">
-                                <div v-if="form.errors.password" class="text-sm text-red-500 py-2">{{ form.errors.password }}</div>    
-                        </div>
-                        <div>
-                            <label class="text-gray-600 font-bold inline-block pb-2" for="password">Confirm Password</label>
-                            <input 
-                                v-model="form.password_confirmation"
-                                class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" 
-                                type="password" name="password_confirmation" 
-                                placeholder="******">
-                            <div v-if="form.errors.password_confirmation" class="text-sm text-red-500 py-2">{{ form.errors.password_confirmation }}</div>    
-                        </div>
+    <Layout>
+        <div class="bg-[#F9FAFB] h-screen w-screen flex items-center">
+            <div class="h-max mx-auto flex flex-col items-center">
+                <h1 class="text-xl font-bold text-center pb-10">Reset your password</h1>
+                    <form @submit.prevent="submit">
+                        <div class="bg-white shadow-xl p-10 flex flex-col gap-4 text-sm">
+                            <div>
+                                <label class="text-gray-600 font-bold inline-block pb-2" for="email">Email</label>
+                                <input
+                                    v-model="form.email" 
+                                    class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" 
+                                    type="email" name="email" 
+                                    placeholder="example@mail.com">
+                                <div v-if="form.errors.email" class="text-sm text-red-500 py-2">{{ form.errors.email }}</div>    
+                            </div>
+                            <div>
+                                <label class="text-gray-600 font-bold inline-block pb-2" for="password">New Password</label>
+                                <input 
+                                    v-model="form.password"
+                                    class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" 
+                                    type="password" name="password" 
+                                    placeholder="******">
+                                    <div v-if="form.errors.password" class="text-sm text-red-500 py-2">{{ form.errors.password }}</div>    
+                            </div>
+                            <div>
+                                <label class="text-gray-600 font-bold inline-block pb-2" for="password">Confirm Password</label>
+                                <input 
+                                    v-model="form.password_confirmation"
+                                    class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" 
+                                    type="password" name="password_confirmation" 
+                                    placeholder="******">
+                                <div v-if="form.errors.password_confirmation" class="text-sm text-red-500 py-2">{{ form.errors.password_confirmation }}</div>    
+                            </div>
 
-                        <div>
-                            <input 
-                                class="bg-[#4F46E5] w-full py-2 rounded-md text-white font-bold cursor-pointer hover:bg-[#181196]" 
-                                type="submit" 
-                                value="Reset your password" >
+                            <div>
+                                <input 
+                                    class="bg-[#4F46E5] w-full py-2 rounded-md text-white font-bold cursor-pointer hover:bg-[#181196]" 
+                                    type="submit" 
+                                    value="Reset your password" >
+                            </div>
+                        
                         </div>
-                       
-                    </div>
-                </form>
+                    </form>
+            </div>
         </div>
-    </div>
+    </Layout>
 </template>
 
 <script setup>
@@ -71,5 +73,14 @@ const submit = () => {
     });
 };
 
+</script>
+
+<script>
+import Layout from './Layouts/Layout.vue'
+
+export default {
+  layout: (h, page) => h(Layout, [page]),
+  layout: Layout,
+}
 </script>
 
