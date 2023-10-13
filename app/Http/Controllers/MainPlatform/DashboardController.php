c8545893a0da238c3de25188b025c886dbe336bc
Slobodan Veselinov <slobodan.veselinov@gmail.com>
2023-10-13 23:11:53 +0200

    unnecessary files deleted

--- app/Http/Controllers/MainPlatform/DashboardController.php
+++ app/Http/Controllers/MainPlatform/DashboardController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\MainPlatform;
 
-use App\Http\Controllers\Controller;
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
